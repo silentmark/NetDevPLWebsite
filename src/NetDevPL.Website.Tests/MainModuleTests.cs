591aa78f88c4e82a219d3a005c572c31cb08abad
Pawel Klimczyk <pawel@klimczyk.pl>
2014-06-23 13:49:59 +0200

    Sample project structure + sample test

---
+++ src/NetDevPL.Website.Tests/MainModuleTests.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainModuleTests.cs" project="NetDevPL.Website.Tests" date="2014-06-23 13:29">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+namespace NetDevPL.Website.Tests
+{
+    using NUnit.Framework;
+
+    using Nancy;
+    using Nancy.Testing;
+
+    using NetDevPL.Website.Modules;
+
+    [TestFixture]
+    public class MainModuleTests
+    {
+        [Test]
+        public void MainModule_accessingMainPage_shouldReturnPage()
+        {
+            // Arrange
+            var browser = new Browser(with => with.Module<MainModule>());
+
+            // Act
+            var result = browser.Get("/", ctx => ctx.HttpRequest());
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            StringAssert.Contains("Hello .NET Developers Poland!", result.Body.AsString());
+        }
+    }
+}
---
+++ src/NetDevPL.Website/Modules/MainModule.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainModule.cs" project="NetDevPL.Website" date="2014-06-23 13:24">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+namespace NetDevPL.Website.Modules
+{
+    using Nancy;
+
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = parameters => "Hello .NET Developers Poland!";
+        }
+    }
+}
