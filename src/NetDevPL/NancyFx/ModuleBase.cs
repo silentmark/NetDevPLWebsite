5c0ee04571ee50e950db209c0072adb5be0313d9
Maciek Misztal <mmisztal1980@gmail.com>
2014-06-29 23:40:43 +0200

    refactored modules

---
+++ src/NetDevPL/NancyFx/ModuleBase.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using System.Reflection;
+
+namespace NetDevPL.NancyFx
+{
+    // cr:mmisztal1980
+    public abstract class ModuleBase : NinjectModule
+    {
+        public abstract string Key { get; }
+
+        public abstract Assembly DeclaringAssembly { get; }
+    }
+}
