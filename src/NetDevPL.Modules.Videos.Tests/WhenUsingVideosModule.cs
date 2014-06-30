900c56f31d50b40c76d302374c0db8e7288dc70e
Maciek Misztal <mmisztal1980@gmail.com>
2014-06-30 18:17:03 +0200

    video module tests enabled

---
+++ src/NetDevPL.Modules.Videos.Tests/WhenUsingVideosModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Testing;
+using NetDevPL.AspNet;
+using NUnit.Framework;
+
+namespace NetDevPL.Modules.Videos.Tests
+{
+    [TestFixture]
+    public class WhenUsingHomeModule
+    {
+        [Test]
+        public void ShouldReturnHttpOkForDefaultRoute()
+        {
+            using (var bootstrapper = new Bootstrapper())
+            {
+                var browser = new Browser(bootstrapper);
+                var result = browser.Get("/videos", with => with.HttpRequest());
+
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            }
+        }
+    }
+}
